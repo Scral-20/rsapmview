3835ae073e6a0ab2fce4566bcdb96b57c45a7552
wubdut <981128587@qq.com>
2018-09-03 14:18:28 +0800

    180903

--- pages/probes/mysql/basicinfo/mysqlbasicinfo.controller.js
+++ pages/probes/mysql/basicinfo/mysqlbasicinfo.controller.js
@@ -12,7 +12,10 @@
             function ($scope, $http, $state, $stateParams, $window, authService) {
 
                 $scope.key = $stateParams.key;
+                $scope.currentPeriod = '1h';
+
                 $scope.getData = function () {
+
                     $http.get(
                         authService.getURL() + "/basicinfo/" + $stateParams.key,
                         {headers : authService.createAuthorizationTokenHeader()}
@@ -20,43 +23,17 @@
                         // console.log("yes");
                         console.log(response.data);
                         var data = response.data;
-
-                        $scope.applicationName = data.applicationName;
-                        $scope.agentVersion = data.agentVersion;
-                        $scope.agentType = data.agentId;
-                        $scope.pid = data.pid;
-                        $scope.hostName = data.hostName;
-                        $scope.jvmInfo = data.jvmInfo;
-                        $scope.ip = data.ip;
-                        $scope.startTimestamp = data.startTimestamp;
-                        $scope.serverMetaData = data.serverMetaData;
-                        $scope.status = data.status;
-                        $scope.currentServiceInfo = [];
-                        $scope.currentPeriod = '30m';
-                        for (var i = 0; i < data.serverMetaData.serviceInfos.length; i++) {
-                            if (data.serverMetaData.serviceInfos[i].serviceLibs.length > 0) {
-                                $scope.currentServiceInfo = data.serverMetaData.serviceInfos[i];
-                                break;
-                            }
-                        }
                     }, function () {
                         console.log("basicinfo no data");
                     });
 
                 };
 
-                $scope.selectServiceInfo = function(serviceInfo) {
-                    // alert(serviceInfo);
-                    if (serviceInfo.serviceLibs.length > 0) {
-                        $scope.currentServiceInfo = serviceInfo;
-                    }
-                };
-
                 $scope.setPeriod = function ( period ) {
                     $scope.currentPeriod = period;
                 };
 
-                $scope.getData();
+                // $scope.getData();
 
             }
         ]);
