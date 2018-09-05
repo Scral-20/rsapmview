fa38cd61c2fb16464837a5c6213a571a8f6e0469
wubdut <981128587@qq.com>
2018-09-05 11:35:53 +0800

    180905

--- pages/panel_prometheus/line/line.directive.js
+++ pages/panel_prometheus/line/line.directive.js
@@ -2,25 +2,28 @@
     'use strict';
     /**
      * wub-neu
-     * 2018.07.30
+     * 2018.09.04
      * @group directive
-     * @name lineDiagram
+     * @name linePrometheusDiagram
      * @class
      */
     angular.module('inspinia')
-        .directive('lineDiagram', [function() {
+        .directive('linePrometheusDiagram', [function() {
             return {
                 restrict: 'E',
-                templateUrl: 'pages/panel/line/line.html',
+                templateUrl: 'pages/panel_prometheus/line/line.html',
                 replace: true,
                 scope: {
                     currentPeriod: "@"
                 },
                 controller: function ($scope, $http, $element, $attrs, AuthService) {
                     $scope._id =  '_' + Math.random().toString(36).substr(2, 9);
-                    $scope.title = "";
-                    $scope.columnsSelected = $attrs.columnsSelected;   //在属性columns中选择需要显示的折线
-                    $scope.tickcount = $attrs.tickcount; //x轴显示标签数量设置
+                    $scope.localtitle = $attrs.localtitle;
+                    if ($scope.localtitle === "undefined") $scope.localtitle ="";
+                    $scope.ylabel = $attrs.ylabel;
+                    $scope.ymin = $attrs.ymin;
+                    $scope.ymax = $attrs.ymax;
+
                     $scope.chartData = {};
                     $scope.c3Axis = {};
                     $scope.c3Data = {
@@ -30,20 +33,17 @@
                     console.log("URL: " + AuthService.getURL() + $attrs.url);
 
                     $scope.getData = function (period) {
+                        var getUrl = AuthService.getURL() + $attrs.url + $attrs.name + "/" + "1h";
+                        console.log(getUrl);
                         $http.get(
-                            AuthService.getURL() + $attrs.url + period
+                            getUrl
                             // {headers : authService.createAuthorizationTokenHeader()}
                         ).then(function (response) {
                             console.log(response.data);
                             $scope.chartData = response.data;
-                            if (typeof($scope.chartData.title) !== "undefined"){
-                                $scope._id = $scope.chartData.title.replace(/ /g, "_")+$scope._id;
-                                $scope.title = $scope.chartData.title;
-                            }
 
-                            $scope.List = getList($scope.chartData);
-                            $scope.c3Axis = transAxis($scope.chartData);
-                            $scope.c3Data = transData($scope.chartData,$scope.List);
+                            // $scope.c3Axis = transAxis($scope.chartData);
+                            // $scope.c3Data = transData($scope.chartData);
                         }, function () {
                             console.log("lineDiagram no data");
                         });
@@ -56,27 +56,26 @@
                             height: 40,
                             tick: {
                                 format: '%H:%M:%S     %Y/%m/%d',
-                                //周报count：14/2=7
                                 count: tickCount($scope.tickcount)
                             },
                             padding: {left:0, right:0}
                         };
                         var y = {};
                         if (typeof(chartData.yLabel) !== "undefined") {
-                            y['label'] = $scope.chartData.yLabel;
+                            y['label'] = $scope.ylabel;
                         }
                         if (typeof(chartData.min) !== "undefined") {
-                            y['min'] = $scope.chartData.min;
+                            y['min'] = $scope.ymin;
                         }
                         if (typeof(chartData.max) !== "undefined") {
-                            y['max'] = $scope.chartData.max;
+                            y['max'] = $scope.ymax;
                         }
                         y['padding'] = {top:0, bottom:0};
                         res['y'] = y;
                         return res;
                     }
 
-                    function transData(chartData,List) {
+                    function transData(chartData) {
                         var res = {
                             x: 'x',
                             columns: [],
@@ -88,9 +87,6 @@
                         }
 
                         for (var i = 0; i < chartData.columns.length; i++) {
-                            if( !ifItemInList(chartData.columns[i].key,List)){
-                                continue;
-                            }
                             var tmp = [];
                             tmp.push(chartData.columns[i].key);
                             for (var j = 0; j < chartData.columns[i].value.length; j++) {
@@ -111,37 +107,6 @@
                             res.groups = chartData.groups;
                         }
                         return res;
-                    }
-
-                    //list列的即为所选的数据key值
-                    function ifItemInList(item,list) {
-                        for(var i = 0; i < list.length; i++){
-                            if(item === list[i]){
-                                return true
-                            }
-                        }
-                        return false
-                    }
-
-                    //若标签columns属性未写，则默认获取全部key与value,否则按columns属性获取数据
-                    function getList(chartData) {
-                        var List=[];
-                        if (typeof ($scope.columnsSelected)==="undefined" || $scope.columnsSelected===""){
-                            for(var i =0; i< chartData.columns.length; i++){
-                                List[i]= chartData.columns[i].key;
-                            }
-                        }else {
-                            List=$scope.columnsSelected.split(',');
-                        }
-                        return List;
-                    }
-
-                    //获取x轴坐标显示数量限制
-                    function tickCount(tickCount) {
-                        if(tickCount === "undefined" || tickCount === ""){
-                            return ''
-                        }
-                        return tickCount
                     }
 
                 },
