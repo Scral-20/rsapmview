1c14791f0eda1f15c3998908aa59463121bbaba4
Scral-20 <582345637@qq.com>
2018-09-19 15:26:13 +0800

    09/19

--- pages/topview/topview.controller.js
+++ pages/topview/topview.controller.js
@@ -18,13 +18,18 @@
                     authService.getURL() + "/topview",
                     {headers : authService.createAuthorizationTokenHeader()}
                 ).then(function (response) {
-                    console.log("yes");
-                    nodes = response.data.nodes;
-                    console.log(response.data.nodes);
-                    links = response.data.links;
-                    console.log(response.data.links);
-                    $scope.model = new go.GraphLinksModel(nodes,links);
-                    $scope.model.selectedNodeData = null;
+                    console.log('code:'+response.data.code);
+                    if (response.data.code === 200) {
+                        console.log("yes");
+                        nodes = response.data.message.nodes;
+                        console.log(response.data.message.nodes);
+                        links = response.data.message.links;
+                        console.log(response.data.message.links);
+                        $scope.model = new go.GraphLinksModel(nodes, links);
+                        $scope.model.selectedNodeData = null;
+                    } else {
+                        sweetAlert('code:' + response.data.code + ' \nPlease check code');
+                    }
                 }, function () {
                     console.log("topview no data");
                     // $state.go('login');
